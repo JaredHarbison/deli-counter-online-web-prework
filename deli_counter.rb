7f404ed2d9eb23091d6cd72d6b4f901f4498fbd5
Jared Harbison <jared.harbison@gmail.com>
2018-12-30 23:50:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,4 +15,6 @@ end
 def take_a_number (array, name)
   array.push (name)
   position = array.index (name)
-  
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
